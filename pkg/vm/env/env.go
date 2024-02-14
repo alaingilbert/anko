25a2fa8184ff5174098272a9e081f59ad3252113
Alain Gilbert <alain.gilbert.15@gmail.com>
2024-02-13 20:57:49 -0800

    cleanup code

--- pkg/vm/env/env.go
+++ pkg/vm/env/env.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/alaingilbert/anko/pkg/utils"
+	vmUtils "github.com/alaingilbert/anko/pkg/vm/utils"
 	"github.com/alaingilbert/mtx"
 	"reflect"
 	"sort"
@@ -155,29 +156,9 @@ func (e *Env) String() string {
 			valuesArr = append(valuesArr, []string{symbol, fmt.Sprintf("%#v", value)})
 			return
 		}
-		numIn := value.Type().NumIn()
-		inParams := make([]string, numIn)
-		for i := 0; i < numIn; i++ {
-			inParams[i] = value.Type().In(i).String()
-			inParams[i] = replaceInterface(inParams[i])
+		if value.Kind() == reflect.Func {
+			valuesArr = append(valuesArr, []string{symbol, vmUtils.FormatFunc(value)})
 		}
-		numOut := value.Type().NumOut()
-		outParams := make([]string, numOut)
-		for i := 0; i < numOut; i++ {
-			outParams[i] = value.Type().Out(i).String()
-			outParams[i] = replaceInterface(outParams[i])
-		}
-
-		inParamsStr := utils.Ternary(numIn > 0, strings.Join(inParams, ", "), "")
-		sign := fmt.Sprintf("func(%s)", inParamsStr)
-		if numOut > 0 {
-			outParamsStr := strings.Join(outParams, ", ")
-			if numOut > 1 {
-				outParamsStr = fmt.Sprintf("(%s)", outParamsStr)
-			}
-			sign += fmt.Sprintf(" %s", outParamsStr)
-		}
-		valuesArr = append(valuesArr, []string{symbol, sign})
 	})
 
 	typesArr := make([][]string, 0)
---
+++ pkg/vm/utils/utils.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/alaingilbert/anko/pkg/utils"
+	"reflect"
+	"strings"
+)
+
+func FormatFunc(value reflect.Value) string {
+	if value.Kind() == reflect.Func {
+		replaceInterface := func(in string) string { return strings.ReplaceAll(in, "interface {}", "any") }
+		numIn := value.Type().NumIn()
+		inParams := make([]string, numIn)
+		for i := 0; i < numIn; i++ {
+			inParams[i] = value.Type().In(i).String()
+			inParams[i] = replaceInterface(inParams[i])
+		}
+		numOut := value.Type().NumOut()
+		outParams := make([]string, numOut)
+		for i := 0; i < numOut; i++ {
+			outParams[i] = value.Type().Out(i).String()
+			outParams[i] = replaceInterface(outParams[i])
+		}
+
+		inParamsStr := utils.Ternary(numIn > 0, strings.Join(inParams, ", "), "")
+		sign := fmt.Sprintf("func(%s)", inParamsStr)
+		if numOut > 0 {
+			outParamsStr := strings.Join(outParams, ", ")
+			if numOut > 1 {
+				outParamsStr = fmt.Sprintf("(%s)", outParamsStr)
+			}
+			sign += fmt.Sprintf(" %s", outParamsStr)
+		}
+		return sign
+	}
+	return ""
+}

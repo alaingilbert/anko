aea8b2407e6ce721fb6a23f77554c9cbd30f7a01
Alain Gilbert <alain.gilbert.15@gmail.com>
2024-02-14 19:14:11 -0800

    print err pos

--- cmd/anko/anko.go
+++ cmd/anko/anko.go
@@ -303,6 +303,7 @@ func compileAndSave(source, fileName string) error {
 	fileName = strings.Replace(fileName, ankoFileExt, ankoBytecodeExt, 1)
 	out, err := compiler.Compile(source, false)
 	if err != nil {
+		fmt.Println(err.(*parser.Error).Pos)
 		return err
 	}
 	if err := os.WriteFile(fileName, out, 0744); err != nil {

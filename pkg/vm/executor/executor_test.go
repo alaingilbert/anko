ed9b4cb9f10df9d47f5482ee3cf06d9d998c1cb4
Alain Gilbert <alain.gilbert.15@gmail.com>
2024-02-18 13:41:31 -0800

    add test

--- pkg/vm/executor/executor_test.go
+++ pkg/vm/executor/executor_test.go
@@ -2,6 +2,7 @@ package executor
 
 import (
 	"context"
+	"github.com/alaingilbert/anko/pkg/ast"
 	envPkg "github.com/alaingilbert/anko/pkg/vm/env"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -15,4 +16,13 @@ func TestGetCycles(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, int64(2), val)
 	assert.Equal(t, int64(11), e.getCycles())
+}
+
+func TestInvalidInput(t *testing.T) {
+	env := envPkg.NewEnv()
+	e := NewExecutor(&Config{Env: env})
+	stmt := &ast.ForStmt{}
+	val, err := e.Run(context.Background(), stmt)
+	assert.ErrorIs(t, err, ErrInvalidInput)
+	assert.Equal(t, nil, val)
 }

3462b7ea34320985534e8bbcf75c20f229885b61
Alain Gilbert <alain.gilbert.15@gmail.com>
2024-02-18 23:41:11 -0800

    cleanup

--- pkg/vm/utils/utils.go
+++ pkg/vm/utils/utils.go
@@ -47,3 +47,19 @@ func FormatValue(value reflect.Value) string {
 	s = ReplaceInterface(s)
 	return s
 }
+
+func KindIsNumeric(kind reflect.Kind) bool {
+	return kind == reflect.Int ||
+		kind == reflect.Int8 ||
+		kind == reflect.Int16 ||
+		kind == reflect.Int32 ||
+		kind == reflect.Int64 ||
+		kind == reflect.Uint ||
+		kind == reflect.Uint8 ||
+		kind == reflect.Uint16 ||
+		kind == reflect.Uint32 ||
+		kind == reflect.Uint64 ||
+		kind == reflect.Uintptr ||
+		kind == reflect.Float32 ||
+		kind == reflect.Float64
+}

3d5ed530fce34726aeec8bfa961d0b32e2819aad
Alain Gilbert <alain.gilbert.15@gmail.com>
2024-02-20 08:26:34 -0800

    cleanup

--- pkg/vm/vm_test.go
+++ pkg/vm/vm_test.go
@@ -1704,7 +1704,7 @@ func TestTypedValues(t *testing.T) {
 		{Script: `a = 1; a = "1"`, RunOutput: "1"},
 		{Script: `a := 1; a = "1"`, RunError: vmUtils.ErrTypeMismatch, Name: ""},
 		{Script: `a := 1; delete("a"); a = "1"`, RunOutput: "1"},
-		{Script: `a := 1; a=2; a = "1"`, RunError: vmUtils.ErrTypeMismatch, Name: ""},
+		{Script: `a := 1; a = 2; a = "1"`, RunError: vmUtils.ErrTypeMismatch, Name: ""},
 	}
 	for _, tt := range tests {
 		t.Run(tt.Name, func(t *testing.T) { runTest(t, tt, &Options{DefineImport: true, ImportCore: true, ResetEnv: true}) })
